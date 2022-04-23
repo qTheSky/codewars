1b304597c6070759619f2aad5d278a1d01fb55bf
qTheSky <smirnov.mic@yandex.ru>
2022-04-23 12:51:59 +0300

    Create Thinkful - Logic Drills Traffic light.js

---
+++ 8 kyu/Thinkful - Logic Drills Traffic light.js
@@ -0,0 +1,11 @@
+function updateLight(current) {
+    if (current === "green"){
+      return "yellow";
+    }
+    if (current === "yellow"){
+      return "red";
+    }
+    if (current === "red"){
+      return "green";
+    }
+  }
